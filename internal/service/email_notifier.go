2f5c9bbd73a1908cea5e498936850f42f6c803ca
Neratus <andrey.fed314@gmail.com>
2025-07-29 17:18:33 +0300

    Added email notifier

---
+++ internal/service/email_notifier.go
@@ -0,0 +1,82 @@
+package service
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"text/template"
+
+	"github.com/bmstu-itstech/scriptum-back/internal/domain/scripts"
+	"gopkg.in/gomail.v2"
+)
+
+type EmailNotifier struct {
+	from     string
+	password string
+	host     string
+	port     int
+}
+
+func NewEmailNotifier(from, password, host string, port int) (*EmailNotifier, error) {
+	return &EmailNotifier{from, password, host, port}, nil
+}
+
+type EmailTemplateData struct {
+	StartedAt    string
+	Code         int
+	OutputValues []string
+	ErrorMessage string
+}
+
+const templateFile = "resources/template/email_template.html"
+
+func (e *EmailNotifier) Notify(_ context.Context, j *scripts.Job, email scripts.Email) error {
+	res, err := j.Result()
+	if err != nil {
+		return err
+	}
+
+	values := res.Output()
+	rawValues := make([]string, len(values))
+	for i, v := range values {
+		rawValues[i] = v.String()
+	}
+
+	data := EmailTemplateData{
+		StartedAt:    j.CreatedAt().Format("02.01.2006 15:04:05"),
+		Code:         int(res.Code()),
+		OutputValues: rawValues,
+	}
+
+	if msg := res.ErrorMessage(); msg != nil {
+		data.ErrorMessage = *msg
+	}
+
+	tmplBytes, err := os.ReadFile(templateFile)
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := template.New("email").Parse(string(tmplBytes))
+	if err != nil {
+		return err
+	}
+
+	var body bytes.Buffer
+	if err := tmpl.Execute(&body, data); err != nil {
+		return err
+	}
+
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", e.from)
+	msg.SetHeader("To", string(email))
+	msg.SetHeader("Subject", "Результат выполнения")
+	msg.SetBody("text/html", body.String())
+
+	dialer := gomail.NewDialer(e.host, e.port, e.from, e.password)
+	if err := dialer.DialAndSend(msg); err != nil {
+		return err
+	}
+
+	return nil
+}
