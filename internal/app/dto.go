d00299dc2d3b3f248972da698b160b546c3dabac
Neratus <andrey.fed314@gmail.com>
2025-07-30 10:54:50 +0300

    Chore: added launch handler

--- internal/app/dto.go
+++ internal/app/dto.go
@@ -30,18 +30,12 @@ type ValueDTO struct {
 	Data string
 }
 
-type ResultDTO struct {
-	Code   int
-	Output []ValueDTO
-	ErrMsg *string
-}
-
 type JobDTO struct {
 	JobID        int64
 	OwnerID      int64
+	ScriptID     int64
 	Input        []ValueDTO
 	State        string
-	Result       *ResultDTO
 	CreatedAt    time.Time
 	FinishedAt   *time.Time
 	NeedToNotify bool
@@ -75,12 +69,48 @@ func ScriptToDTO(_ scripts.Script) ScriptDTO {
 	return ScriptDTO{}
 }
 
-func DTOToJob(_ JobDTO) (*scripts.Job, error) {
-	return nil, nil
+func DTOToJob(j JobDTO) (*scripts.Job, error) {
+	values, err := DTOToValues(j.Input)
+	if err != nil {
+		return nil, err
+	}
+
+	job, err := scripts.RestoreJob(
+		j.JobID,
+		j.OwnerID,
+		j.ScriptID,
+		j.State,
+		values,
+		nil,
+		j.CreatedAt,
+		j.FinishedAt,
+	)
+
+	return job, err
 }
 
-func DTOToValues(_ []ValueDTO) ([]scripts.Value, error) {
-	return nil, nil
+func ValuesToDTO(values []scripts.Value) ([]ValueDTO, error) {
+	jobValues := make([]ValueDTO, len(values))
+	for i, v := range values {
+		val := ValueDTO{
+			v.Type().String(),
+			v.String(),
+		}
+		jobValues[i] = val
+	}
+	return jobValues, nil
+}
+
+func DTOToValues(values []ValueDTO) ([]scripts.Value, error) {
+	jobValues := make([]scripts.Value, len(values))
+	for i, v := range values {
+		val, err := scripts.NewValue(v.Type, v.Data)
+		if err != nil {
+			return nil, err
+		}
+		jobValues[i] = val
+	}
+	return jobValues, nil
 }
 
 func DTOToFile(_ FileDTO) (*scripts.File, error) {
