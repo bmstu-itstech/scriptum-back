11c1c163dbe4f8c4bab3b2702f0e33a9933ea894
zhikh <zhikh.k@gmail.com>
2025-11-23 20:16:26 +0300

    cleanup

--- internal/domain/scripts/dispatcher.go
+++
@@ -1,7 +0,0 @@
-package scripts
-
-import "context"
-
-type Dispatcher interface {
-	Start(context.Context, *Job, bool) error
-}
--- internal/domain/scripts/errors.go
+++
@@ -1,9 +0,0 @@
-package scripts
-
-import (
-	"errors"
-)
-
-var ErrInvalidInput = errors.New("invalid input")
-var ErrInvalidStateChange = errors.New("invalid state change")
-var ErrPermissionDenied = errors.New("permission denied")
